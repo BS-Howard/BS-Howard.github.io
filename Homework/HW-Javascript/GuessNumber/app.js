8c89ef521d3e897e4945d99ef944ffdfcafe13d6
BS-Howard <howard199887@gmail.com>
2021-06-08 12:36:20 +0800

    NumberGuest

--- Homework/HW-Javascript/GuessNumber/app.js
+++ Homework/HW-Javascript/GuessNumber/app.js
@@ -2,7 +2,7 @@ const randomButton = document.querySelector(".number > button");
 const inputText = document.querySelector("input");
 let h1 = document.querySelector(".show h1");
 randomButton.addEventListener("click", (e) => {
-  randomButton. disabled = true;
+  randomButton.disabled = true;
   //產生亂數
   function getRandom(min, max) {
     return Math.floor(Math.random() * max + min);
@@ -17,32 +17,32 @@ randomButton.addEventListener("click", (e) => {
     });
   });
 
-  var min = 0
-  var max = 100
+  var min = 0;
+  var max = 100;
   //開始判斷數字
   const checkButton = document.querySelector("#check");
   checkButton.addEventListener("click", () => {
-    if (inputText.value > max && inputText.value < min){
-        alert("請輸入正確範圍內的數字")
-        inputText.value = ""
-        return
-    }
-    if (inputText.value == randomNumber) {
-      alert("Bingo");
-      h1.innerText = "0~100";
+    if (inputText.value > max || inputText.value < min) {
+      alert("請輸入正確範圍內的數字");
       inputText.value = "";
-      window.location.replace(window.location.href);
-    } else if(inputText.value < randomNumber){
+      return;
+    } else {
+      if (inputText.value == randomNumber) {
+        alert("Bingo");
+        h1.innerText = "0~100";
+        inputText.value = "";
+        window.location.replace(window.location.href);
+      } else if (inputText.value < randomNumber) {
         h1.innerText = `${inputText.value}~${max}`;
-        min = inputText.value
-        inputText.value = ""
-    }else{
+        min = inputText.value;
+        inputText.value = "";
+      } else {
         h1.innerText = `${min}~${inputText.value}`;
-        max = inputText.value
-        inputText.value = ""
+        max = inputText.value;
+        inputText.value = "";
+      }
     }
   });
-
 
   // 按鈕取消輸入
   const deleteButton = document.querySelector("#delete");
