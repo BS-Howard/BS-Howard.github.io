be143fcc88fc12a65821385304ddf183543542d8
BS-Howard <howard199887@gmail.com>
2021-06-11 14:42:57 +0800

    Get AB

--- Homework/HW-Javascript/GuessAB/app.js
+++ Homework/HW-Javascript/GuessAB/app.js
@@ -1,6 +1,14 @@
 const challenge = [
-  {name: "easy",num : 4,str:"一共四個不相同數字，請開始挑戰"},
-  {name: "hard",num : 5,str:"一共五個不相同數字，請開始挑戰"}
+  {name:"難度1",num : 1,str:"一共一個不相同數字，按下開始挑戰後開始遊戲!"},
+  {name:"難度2",num : 2,str:"一共兩個不相同數字，按下開始挑戰後開始遊戲!"},
+  {name:"難度3",num : 3,str:"一共三個不相同數字，按下開始挑戰後開始遊戲!"},
+  {name:"難度4",num : 4,str:"一共四個不相同數字，按下開始挑戰後開始遊戲!"},
+  {name:"難度5",num : 5,str:"一共五個不相同數字，按下開始挑戰後開始遊戲!"},
+  {name:"難度6",num : 6,str:"一共六個不相同數字，按下開始挑戰後開始遊戲!"},
+  {name:"難度7",num : 7,str:"一共七個不相同數字，按下開始挑戰後開始遊戲!"},
+  {name:"難度8",num : 8,str:"一共八個不相同數字，按下開始挑戰後開始遊戲!"},
+  {name:"難度9",num : 9,str:"一共九個不相同數字，按下開始挑戰後開始遊戲!"},
+  {name:"難度10",num : 10,str:"一共十個不相同數字，按下開始挑戰後開始遊戲!"},
 ]
 
 const startButton = document.querySelector("#start");
@@ -11,6 +19,7 @@ const challengeButton = document.querySelectorAll(".challenge button")
 
 challengeButton.forEach(item=>{
   item.addEventListener("click",(e)=>{
+
     //難度說明
     challenge.forEach(x=>{
       if(item.innerText == x.name){
@@ -18,27 +27,25 @@ challengeButton.forEach(item=>{
         challengeNum = x.num;
       }
     })
+
     // 關閉挑戰按鈕
     challengeButton.forEach((x) => {
       x.disabled = true;
     });
+
     //開始按鈕
     startButton.addEventListener("click", () => {
       startButton.disabled = true;
+
       //產生亂數
       function getRandom(min, max) {
         return Math.floor(Math.random() * max + min);
       }
       var list = [];
       while (list.length != challengeNum) {
-        var randomNumber = getRandom(0, 9);
-        if (
-          !list.some((x) => {
-            return x == randomNumber;
-          })
-        ) {
-          list.push(randomNumber);
-        }
+        var randomNumber = getRandom(0, 10);
+        if (!list.some((x) => {return x == randomNumber;})
+        ) { list.push(randomNumber);}
       }
       var randomNum = "";
       list.forEach((x) => {
@@ -50,30 +57,24 @@ challengeButton.forEach(item=>{
       const showAnswer = document.querySelector("#showAnswer");
       showAnswer.addEventListener("click", () => {
         let answerDiv = document.createElement("div");
-        answerDiv.style.backgroundColor = "#fa0";
-        answerDiv.innerText = `Answer is ${randomNum}`;
+        answerDiv.innerHTML = `<h5>Answer</h5> <span>${randomNum}</span>`;
+        answerDiv.firstChild.style.backgroundColor = "#fa0";
         section.append(answerDiv);
+        showAnswer.disabled = true
       });
     
       //猜數字
       guessButton.addEventListener("click", () => {
+
         //判斷輸入的正確性
         function isNumber(input) {
           let result = !!Number(input);
           return result;
         }
     
-        function isRepeat(input) {
+        function isRepeat(input){
           result = input.split("");
-          var bool = false;
-          result.forEach((x, index) => {
-            for (let i = 0; i < challengeNum; i++) {
-              if (x == result[i] && index != i) {
-                bool = true;
-              }
-            }
-          });
-          return bool;
+          return result.sort().some((x,index)=>x==result[index+1])
         }
     
         if (
@@ -87,30 +88,21 @@ challengeButton.forEach(item=>{
         }
     
         //幾A幾B
-        var countA = 0,
-          countB = 0;
-        for (let i = 0; i < challengeNum; i++) {
-          if (userGuess.value[i] == randomNum[i]) {
-            countA += 1;
-          } else {
-            for (let j = 0; j < challengeNum; j++) {
-              if (userGuess.value[i] == randomNum[j]) {
-                countB += 1;
-              }
-            }
-          }
-        }
+        let guess = userGuess.value.split('')
+        let countA = guess.filter((x,index) => x == list[index]).length
+        let countB = guess.filter(x => Number(list.indexOf(Number(x))) !== -1 ).length -countA
         let div = document.createElement("div");
-        div.innerText = `${countA}A${countB}B - ${userGuess.value}`;
+        div.innerHTML = `<h5>${countA}A${countB}B</h5>  <span>${userGuess.value}</span>`;
         section.append(div);
     
         //判斷是否獲勝
         if (userGuess.value == randomNum) {
-          div.style.backgroundColor = "green";
+          div.firstChild.style.backgroundColor = "green";
           userGuess.value = "";
           alert("恭喜成功猜中!");
+          userGuess.disabled = true
         } else {
-          div.style.backgroundColor = "red";
+          div.firstChild.style.backgroundColor = "red";
           userGuess.value = "";
         }
       });
