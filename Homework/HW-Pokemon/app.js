4183cedb87040c6d95998b825edb7a2762a112ff
BS-Howard <howard199887@gmail.com>
2021-06-11 11:23:34 +0800

    pokemon

--- Homework/HW-Pokemon/app.js
+++ Homework/HW-Pokemon/app.js
@@ -5,40 +5,87 @@ const removeOneBtn = document.querySelector("#removeOne");
 const timerBtn = document.querySelector("#timer");
 const stopTimerBtn = document.querySelector("#stopTimer");
 const container = document.querySelector("#container");
+const orderAddBtn = document.querySelector("#orderAdd");
 
 let intervalId;
 let count = 1;
+let record = [];
 
 loadAllBtn.addEventListener("click", () => {
   for (count; count < 898; count++) {
-    let pokemonImg = document.createElement("img");
-    if (count < 100) {
-      count = count.toString().padStart(3, "0");
-    }
-    pokemonImg.setAttribute(
-      "src",
-      `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${count}.png`
-    );
-    container.appendChild(pokemonImg);
-    slideOut(pokemonImg, "slide");
+    createImg();
   }
-  RemoveEachImg();
 });
 
 removeAllBtn.addEventListener("click", () => {
   let pokemonImg = document.querySelectorAll("#container img");
   pokemonImg.forEach((x) => {
-    slideOut(x, "slideOut");
-    x.addEventListener("animationend", () => {
-      container.removeChild(x);
-    });
+    DoSlideOut(x);
   });
   count = 1;
 });
 
 plusOneBtn.addEventListener("click", () => {
-  if (count < 1) {
-    count = 1;
+  createImg();
+  count++;
+});
+
+removeOneBtn.addEventListener("click", () => {
+  if (count == 1) {
+    return;
+  } else {
+    let pokemonImg = document.querySelectorAll("#container img");
+    DoSlideOut(pokemonImg[pokemonImg.length - 1]);
+    count--;
+  }
+});
+
+timerBtn.addEventListener("click", () => {
+  intervalId = setInterval(() => {
+    createImg();
+    count++;
+  }, 500);
+});
+
+stopTimerBtn.addEventListener("click", () => {
+  clearInterval(intervalId);
+});
+
+orderAddBtn.addEventListener("click", () => {
+  if (record.length == 0) {
+    return;
+  }
+  let pokemonImg = document.createElement("img");
+  let num = record.shift();
+  pokemonImg.setAttribute(
+    "src",
+    `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${num}.png`
+  );
+  slideOut(pokemonImg, "slide");
+  pokemonImg.addEventListener("click", (e) => {
+    slideOut(e.target, "slideOut");
+    e.target.addEventListener("animationend", (event) => {
+      removed(event.target);
+    });
+  });
+
+  let bool = true;
+  container.childNodes.forEach((item) => {
+    let x = Number(item.getAttribute("src").substr(-7, 3));
+    console.log(num, x);
+    if (x > num && bool == true) {
+      container.insertBefore(pokemonImg, item);
+      bool = false;
+    }
+    if (bool = true && num > x) {
+      container.appendChild(pokemonImg)
+    }
+  });
+});
+
+function createImg() {
+  if (count > 898) {
+    return;
   }
   let pokemonImg = document.createElement("img");
   if (count < 100) {
@@ -48,68 +95,41 @@ plusOneBtn.addEventListener("click", () => {
     "src",
     `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${count}.png`
   );
-  container.appendChild(pokemonImg);
   slideOut(pokemonImg, "slide");
-  RemoveEachImg();
-  count++;
-});
-
-removeOneBtn.addEventListener("click", () => {
-  if (count == 1) {
-    return;
-  } else {
-    let pokemonImg = document.querySelectorAll("#container img");
-    let lastChild = pokemonImg[pokemonImg.length - 1];
-    slideOut(lastChild, "slideOut");
-    lastChild.addEventListener("animationend", () => {
-      container.removeChild(lastChild);
+  pokemonImg.addEventListener("click", (e) => {
+    slideOut(e.target, "slideOut");
+    e.target.addEventListener("animationend", (event) => {
+      removed(event.target);
     });
-    count--;
-  }
-});
-
-timerBtn.addEventListener("click", () => {
-  set();
-});
-
-stopTimerBtn.addEventListener("click", () => {
-  clearInterval(intervalId);
-  RemoveEachImg();
-});
-
-function set() {
-  intervalId = setInterval(() => {
-    let pokemonImg = document.createElement("img");
-    if (count < 100) {
-      count = count.toString().padStart(3, "0");
-    }
-    pokemonImg.setAttribute(
-      "src",
-      `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${count}.png`
-    );
-    container.appendChild(pokemonImg);
-    slideOut(pokemonImg, "slide");
-    count++;
-  }, 500);
+  });
+  container.appendChild(pokemonImg);
 }
 
-function RemoveEachImg() {
-  var eachImg = document.querySelectorAll("#container img");
-  if (eachImg.length == 0) {
-    return;
-  } else {
-    eachImg.forEach((item) => {
-      item.addEventListener("click", (e) => {
-        slideOut(item, "slideOut");
-        item.addEventListener("animationend", () => {
-          item.remove();
-        });
-        count--;
-      });
-    });
+function removed(img) {
+  img.remove();
+  recordNum(img);
+  Doreset();
+}
+
+function DoSlideOut(x) {
+  slideOut(x, "slideOut");
+  x.addEventListener("animationend", () => {
+    container.removeChild(x);
+    Doreset();
+  });
+}
+
+function Doreset() {
+  if (container.children.length == 0) {
+    count = 1;
   }
 }
 
 function slideOut(img, s) {
   img.style.animation = `${s} .3s`;
+}
+
+function recordNum(img) {
+  str = img.src.substr(-7, 3);
+  record.unshift(str);
 }
